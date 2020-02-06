aec5ec71c6ab9d29a5c07287f9bbca1a76f392f0
Maksym Yurchenko <mvy@norse.co>
2020-02-06 12:32:04 +0200

    fixed jquery error

--- bin/main/services/attachmentCleanup/attachmentCleanup.js
+++
@@ -1,6 +0,0 @@
-var contentLib = require('/lib/xp/content');
-
-exports.post = function( req ) {
-    var params = req.params;
-    contentLib.removeAttachment({key: params.cid, name: params.name});
-};
