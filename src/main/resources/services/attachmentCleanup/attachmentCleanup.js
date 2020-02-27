b15f10cf51c18bb0484b7e1111d9944a854a7454
Maksym Yurchenko <mvy@norse.co>
2020-02-27 12:03:03 +0200

    fixed a bug when removing multiple attachments

--- src/main/resources/services/attachmentCleanup/attachmentCleanup.js
+++ src/main/resources/services/attachmentCleanup/attachmentCleanup.js
@@ -1,6 +1,6 @@
-var contentLib = require('/lib/xp/content');
+var contentLib = require("/lib/xp/content");
 
-exports.post = function( req ) {
-    var params = req.params;
-    contentLib.removeAttachment({key: params.cid, name: params.name});
+exports.post = function(req) {
+  var params = req.params;
+  contentLib.removeAttachment({ key: params.cid, name: params.name });
 };
